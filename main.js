1a64e6af85c8debe70c7392845129ccb29a7c47c
Peter Sifuentez <sifuentez_peter@yahoo.com>
2022-09-23 13:29:17 -0500

    Adding Javascript file

---
+++ main.js
@@ -0,0 +1,17 @@
+//console.log("--");
+//console.log("Sup");
+
+let Peter, Jessica
+
+Peter = "Happy";
+Jessica = "Happy";
+
+Peter + Jessica = "Happness"; //I love you mi Amor 
+
+//Shopping cart 
+
+//cart_1
+const item1 = 25;
+const item2 = 50;
+const item3 = 100;
+let total = item1 + item2 + item3; //total items in cart_1
